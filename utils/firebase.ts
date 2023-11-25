7bc6afb73375175febcc5a7dc61aa371b3fc3de3
nasir1196 <nasiruddin.cf@gmail.com>
2023-11-25 16:19:34 +0600

    started next commerce project

---
+++ utils/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDajr5xbE2wa1esAtt3R3xbWsv5BMrRbHI",
+  authDomain: "next-commerce-7be37.firebaseapp.com",
+  projectId: "next-commerce-7be37",
+  storageBucket: "next-commerce-7be37.appspot.com",
+  messagingSenderId: "1032681171120",
+  appId: "1:1032681171120:web:dfbfb85e7ec39ab0fa7375"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
---
+++ utils/prismadb.ts
@@ -0,0 +1,12 @@
+import prisma from "../prisma";
+
+export const connectDB = async () => {
+    try {
+        await prisma.$connect();
+
+    } catch (error: any) {
+        return new Error(error.message)
+    } finally {
+        await prisma.$disconnect()
+    }
+}
